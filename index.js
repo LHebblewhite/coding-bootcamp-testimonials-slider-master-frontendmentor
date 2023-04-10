6abc16a45a6f3e0effe7236cbeaebe147b22f7e8
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-10 16:12:56 +0100

    Initial draft.

---
+++ index.js
@@ -0,0 +1,13 @@
+function findBottomOfTestimonialImage(){
+    let testimonialImage =  document.getElementById("testimonialImage");
+    let testimonialSelector =  document.getElementById("testimonialSelector");
+    let imageBottomCoord = testimonialImage.getBoundingClientRect().bottom;
+    let selectorCoords = testimonialSelector.getBoundingClientRect();
+    let selectorTopCoord = selectorCoords.top;
+    let selectorBottomCoord = selectorCoords.bottom;
+    let selectorOffset = (selectorBottomCoord-selectorTopCoord)/2;
+
+    testimonialSelector.style.top = imageBottomCoord - selectorOffset;
+}
+
+window.addEventListener("resize", findBottomOfTestimonialImage);
