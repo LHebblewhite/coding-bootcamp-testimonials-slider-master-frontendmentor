9b251c9a357900e71518f401940581855e7b3b87
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-18 20:54:04 +0100

    Increment and decrement functions added in Javascript. onclick functionality added to next and previous. Styles changed.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let testimonialSelectorVar = 0;
+let testimonialArray = [["John Tarkpor", "Junior Front-end Developer", "\" If you want to lay the best foundation possible I\’d recommend taking this course.\n"
++ "The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. \"", "./images/image-john.jpg"], ["Tanya Sinclair", "UX Engineer", "\" I\’ve been interested in coding for a while but never taken the jump, until now.\n" 
++ "I couldn\’t recommend this course enough. I\’m now in the job of my dreams and so\n" 
++ "excited about the future. \"", "./images/image-tanya.jpg"]]; 
+
 function findBottomOfTestimonialImage(){
     let testimonialImage = document.getElementById("testimonialImage");
     let testimonialSelector = document.getElementById("testimonialSelector");
@@ -10,4 +16,28 @@ function findBottomOfTestimonialImage(){
     testimonialSelector.style.top = String(imageBottomCoord - selectorOffset) + "px";
 }
 
+function incrementButtonPress(){
+    if(testimonialSelectorVar < testimonialArray.length){
+        testimonialSelectorVar++;
+    } else {
+        testimonialSelectorVar = 0;
+    }
+    document.getElementById("testimonialName") = testimonialArray[testimonialSelectorVar][0];
+    document.getElementById("testimonialRole") = testimonialArray[testimonialSelectorVar][1];
+    document.getElementById("quoteText") = testimonialArray[testimonialSelectorVar][2];
+    document.getElementById("testimonialImage").src = testimonialArray[testimonialSelectorVar][3];
+}
+
+function decrementButtonPress(){
+    if(testimonialSelectorVar > testimonialArray.length){
+        testimonialSelectorVar--;
+    } else {
+        testimonialSelectorVar = testimonialArray.length;
+    }
+    document.getElementById("testimonialName") = testimonialArray[testimonialSelectorVar][0];
+    document.getElementById("testimonialRole") = testimonialArray[testimonialSelectorVar][1];
+    document.getElementById("quoteText") = testimonialArray[testimonialSelectorVar][2];
+}
+
+window.onload = findBottomOfTestimonialImage();
 window.addEventListener("resize", findBottomOfTestimonialImage);
